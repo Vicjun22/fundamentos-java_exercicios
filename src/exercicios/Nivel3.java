1804c9d1797d84303abf148492e91bf5bab5c426
Victor Elias Ross Júnior <vicjun22@gmail.com>
2025-04-14 23:40:05 -0300

    feat: adiciona exercícios de nível 3

---
+++ src/exercicios/Nivel3.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+public class Nivel3 {
+
+    // ----------------------------------------------------------------------------------------------------
+
+    public static boolean executarExercicio1 = false;
+
+    // Encontre o valor binário para o respectivo parâmetro de entrada.
+    // Exemplo: Se o parâmetro de entrada for 5, a resposta deverá ser "101"
+    public String exercicio1(int a) {
+
+        // escreva seu código abaixo:
+        return "";
+    }
+
+    // ----------------------------------------------------------------------------------------------------
+
+    public static boolean executarExercicio2 = false;
+
+    // Encontre o valor inteiro para o respectivo parâmetro de entrada binário.
+    // Exemplo: Se o parâmetro de entrada for "1101", a resposta deverá ser 13
+    public int exercicio2(String binario) {
+
+        // escreva seu código abaixo:
+        return 0;
+    }
+}
